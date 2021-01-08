107d8985196d05c4c93410c1b61c2438fcc061e5
Silvia Giammarinaro <silvia.giammarinaro@gmail.com>
2021-01-08 14:26:21 +0100

    Add files via upload

---
+++ data/load_data.py
@@ -0,0 +1,47 @@
+def load_data(dir, img_size, batch_size, validation_split=0.2, generate_validation=True, generate_test=True):
+
+    # Create datasetes from folders
+
+    #train_dir = '/content/drive/MyDrive/BIOINF/covid-project/COVID-19-xrays-resized/train'
+    #test_dir = '/content/drive/MyDrive/BIOINF/covid-project/COVID-19-xrays-resized/test'
+
+    img_height = img_size[0]
+    img_width = img_size[1]
+
+    train_dir = dir + '/train'
+    test_dir = dir + '/test'
+
+    if generate_validation == True:
+        Train_Gen = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255, validation_split)
+        
+        train_ds = Train_Gen.flow_from_directory(train_dir, 
+                                                    target_size = (img_height, img_width), 
+                                                    batch_size = batch_size, 
+                                                    class_mode = 'binary',
+                                                    subset='training')
+
+        validation_ds = Train_Gen.flow_from_directory(train_dir, 
+                                                        target_size = (img_height, img_width), 
+                                                        batch_size = batch_size, 
+                                                        class_mode = 'binary',
+                                                        subset='validation')
+    else: 
+        Train_Gen = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255)
+        train_ds = Train_Gen.flow_from_directory(train_dir, 
+                                                    target_size = (img_height, img_width), 
+                                                    batch_size = batch_size, 
+                                                    class_mode = 'binary')
+
+    Test_Gen = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255)
+    
+    
+    if generate_validation == True and generate_test == True:
+        test_ds = Test_Gen.flow_from_directory(test_dir, 
+                                                    target_size = (img_height, img_width),
+                                                    class_mode = 'binary', 
+                                                    batch_size = batch_size)
+                                                    
+        return train_ds, validation_ds, test_ds
+
+    else if generate_validation == False and generate_test == False:
+        return train_ds
