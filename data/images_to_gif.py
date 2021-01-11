18114936052bc8c7ffded29c4aa807b09d981dae
Silvia Giammarinaro <silvia.giammarinaro@gmail.com>
2021-01-11 08:38:39 +0100

    Add files via upload

---
+++ data/images_to_gif.py
@@ -0,0 +1,19 @@
+
+import imageio
+import glob
+
+def images_to_gif(img_folder, gif_path)
+
+# Create a gif with generated images from GAN 
+# tutorial found here: https://www.tensorflow.org/tutorials/generative/dcgan#create_a_gif
+
+#path images: '/content/drive/MyDrive/BIOINF/images_GAN/one-class/*.png'
+#gif path: '/content/drive/MyDrive/BIOINF/images_GAN/one-class/gan-one-class.gif'
+
+with imageio.get_writer(gif_path, mode='I') as writer:
+  filenames = glob.glob(img_folder)
+  filenames = sorted(filenames)
+  for filename in filenames:
+    image = imageio.imread(filename)
+    writer.append_data(image)
+
