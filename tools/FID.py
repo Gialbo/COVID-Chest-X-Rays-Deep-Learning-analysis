f2d25e2bf4231f7a4b4abcafad24b1a02ea231dd
Silvia <48796281+sigeek@users.noreply.github.com>
2021-06-11 10:24:05 +0200

    Added function for trails FID

--- tools/FID.py
+++ tools/FID.py
@@ -4,11 +4,12 @@ Bioinformatics, Politecnico di Torino
 Authors: Gilberto Manunza, Silvia Giammarinaro
 """
 
-import numpy
+import numpy as np
 from numpy import cov, trace, iscomplexobj, asarray
 from numpy.random import randint
 from scipy.linalg import sqrtm
 from skimage.transform import resize
+import tensorflow as tf
 
 
 class FID():
@@ -41,5 +42,27 @@ class FID():
         fid = ssdiff + trace(sigma1 + sigma2 - 2.0 * covmean)
         return fid
 
-    
+    def get_FID(batch_size, generative_model, evaluation_model, real_images, n_classes=3, n_trial=5):
+
+        FID_array = np.zeros(trials)
+        for i in n_trails:
+            benchmarkNoise = tf.random.normal([batch_size, generative_model.latent_size])
+            benchmarkLabels = np.random.randint(0, n_classes, batch_size)
+
+            fake_images = generative_model.generator.predict([benchmarkNoise, benchmarkLabels])
+
+            real_scaled_images = self.scale_images(real_images, (299,299,3))
+            fake_images_rgb = tf.image.grayscale_to_rgb(tf.convert_to_tensor(fake_images), name=None)
+            fake_scaled_images = self.scale_images(fake_images_rgb, (299,299,3))
+
+
+            fid = self.calculate_fid(evaluation_model, real_scaled_images, fake_scaled_images)
+            FID_array[i] = fid
+
+        mean_FID = np.mean(FID_array)
+        std_FID = np.std(FID_array)
+
+        return mean_FID, std_FID
+
+                
  
