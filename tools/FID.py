39ea2f13bae575104d344365a9f095c13ea30e26
Silvia <48796281+sigeek@users.noreply.github.com>
2021-06-11 10:29:14 +0200

    Fix FID class

--- tools/FID.py
+++ tools/FID.py
@@ -14,7 +14,14 @@ import tensorflow as tf
 
 class FID():
 
-    def scale_images(images, new_shape):
+    def __init__(self,
+                batch_size=383,
+                n_trials=5):
+
+        self.batch_size = batch_size
+        self.n_trials = n_trials
+
+    def scale_images(self, images, new_shape):
         images_list = list()
         for image in images:
             # resize with nearest neighbor interpolation
@@ -24,7 +31,7 @@ class FID():
         return asarray(images_list)
 
     # calculate frechet inception distance
-    def calculate_fid(model, images1, images2):
+    def calculate_fid(self, model, images1, images2):
         # calculate activations
         act1 = model.predict(images1)
         act2 = model.predict(images2)
@@ -32,7 +39,7 @@ class FID():
         mu1, sigma1 = act1.mean(axis=0), cov(act1, rowvar=False)
         mu2, sigma2 = act2.mean(axis=0), cov(act2, rowvar=False)
         # calculate sum squared difference between means
-        ssdiff = numpy.sum((mu1 - mu2)**2.0)
+        ssdiff = np.sum((mu1 - mu2)**2.0)
         # calculate sqrt of product between cov
         covmean = sqrtm(sigma1.dot(sigma2))
         # check and correct imaginary numbers from sqrt
@@ -42,19 +49,18 @@ class FID():
         fid = ssdiff + trace(sigma1 + sigma2 - 2.0 * covmean)
         return fid
 
-    def get_FID(batch_size, generative_model, evaluation_model, real_images, n_classes=3, n_trial=5):
+    def get_FID(self, generative_model, evaluation_model, real_images, n_classes=3):
 
-        FID_array = np.zeros(trials)
-        for i in n_trails:
-            benchmarkNoise = tf.random.normal([batch_size, generative_model.latent_size])
-            benchmarkLabels = np.random.randint(0, n_classes, batch_size)
+        FID_array = np.zeros(self.n_trials)
+        real_scaled_images = self.scale_images(real_images, (299,299,3))
+        for i in self.n_trials:
+            benchmarkNoise = tf.random.normal([self.batch_size, generative_model.latent_size])
+            benchmarkLabels = np.random.randint(0, n_classes, self.batch_size)
 
             fake_images = generative_model.generator.predict([benchmarkNoise, benchmarkLabels])
-
-            real_scaled_images = self.scale_images(real_images, (299,299,3))
+            
             fake_images_rgb = tf.image.grayscale_to_rgb(tf.convert_to_tensor(fake_images), name=None)
             fake_scaled_images = self.scale_images(fake_images_rgb, (299,299,3))
-
 
             fid = self.calculate_fid(evaluation_model, real_scaled_images, fake_scaled_images)
             FID_array[i] = fid
