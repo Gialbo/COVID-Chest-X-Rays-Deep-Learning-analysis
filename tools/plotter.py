274491e6f98d48159a75e70c3ac3b84757160bb9
Gialbo <gilbertomanunza@gmail.com>
2021-06-17 11:25:48 +0200

    Bug fixing on plotter

--- tools/plotter.py
+++ tools/plotter.py
@@ -33,10 +33,12 @@ def plot_accuracies(history, isMCD=False, selected_run='3'):
     train_acc = history.history["accuracy"]
     valid_acc = history.history["val_accuracy"]
   else:
-    train_acc = history[selected_run].history["accuracy"]
+    train_acc = history[selected_run].history["accuracy" if "accuracy" in history[selected_run].history.keys() else "train_accuracy"]
     valid_acc = history[selected_run].history["val_accuracy"]
 
-  epochs = range(len(train_acc)) 
+  if len(valid_acc) != len(train_acc):
+    train_acc = train_acc[::int(len(train_acc)/len(valid_acc))]
+  epochs = range(len(valid_acc)) 
   plt.figure(figsize=(8,6))
 
   with plt.style.context('fivethirtyeight'):
@@ -53,21 +55,28 @@ def plot_recalls(history, isMCD=False, selected_run='3'):
     fig, axs = plt.subplots(1, 3, figsize = (15, 5))
 
     if isMCD:
-      train_rec_0 = history.history["recall_6"]
-      valid_rec_0 = history.history["val_recall_6"]
-      train_rec_1 = history.history["recall_7"]
-      valid_rec_1 = history.history["val_recall_7"]
-      train_rec_2 = history.history["recall_8"]
-      valid_rec_2 = history.history["val_recall_8"]
+      keys = list(history.history.keys())
+      train_rec_0 = history.history[keys[2]]
+      valid_rec_0 = history.history[keys[10]]
+      train_rec_1 = history.history[keys[3]]
+      valid_rec_1 = history.history[keys[11]]
+      train_rec_2 = history.history[keys[4]]
+      valid_rec_2 = history.history[keys[12]]
     else:
-      train_rec_0 = history[selected_run].history["recall_6"]
-      valid_rec_0 = history[selected_run].history["val_recall_6"]
-      train_rec_1 = history[selected_run].history["recall_7"]
-      valid_rec_1 = history[selected_run].history["val_recall_7"]
-      train_rec_2 = history[selected_run].history["recall_8"]
-      valid_rec_2 = history[selected_run].history["val_recall_8"]
+      keys = list(history[selected_run].history.keys())
+      train_rec_0 = history[selected_run].history[keys[2]]
+      valid_rec_0 = history[selected_run].history[keys[10]]
+      train_rec_1 = history[selected_run].history[keys[3]]
+      valid_rec_1 = history[selected_run].history[keys[11]]
+      train_rec_2 = history[selected_run].history[keys[4]]
+      valid_rec_2 = history[selected_run].history[keys[12]]
 
-    epochs = range(len(train_rec_0)) 
+    if len(valid_rec_0) != len(train_rec_0):
+      train_rec_0 = train_rec_0[::int(len(train_rec_0)/len(valid_rec_0))]
+      train_rec_1 = train_rec_0[::int(len(train_rec_1)/len(valid_rec_1))]
+      train_rec_2 = train_rec_0[::int(len(train_rec_2)/len(valid_rec_2))]
+
+    epochs = range(len(valid_rec_0)) 
 
 
     axs[0].plot(epochs, train_rec_0)
@@ -98,21 +107,27 @@ def plot_precisions(history, isMCD=False, selected_run='3'):
     fig, axs = plt.subplots(1, 3, figsize = (15, 5))
 
     if isMCD:
-      train_pre_0 = history.history["precision_6"]
-      valid_pre_0 = history.history["val_precision_6"]
-      train_pre_1 = history.history["precision_7"]
-      valid_pre_1 = history.history["val_precision_7"]
-      train_pre_2 = history.history["precision_8"]
-      valid_pre_2 = history.history["val_precision_8"]
+      keys = list(history.history.keys())
+      train_pre_0 = history.history[keys[5]]
+      valid_pre_0 = history.history[keys[13]]
+      train_pre_1 = history.history[keys[6]]
+      valid_pre_1 = history.history[keys[14]]
+      train_pre_2 = history.history[keys[7]]
+      valid_pre_2 = history.history[keys[15]]
     else:
-      train_pre_0 = history[selected_run].history["precision_6"]
-      valid_pre_0 = history[selected_run].history["val_precision_6"]
-      train_pre_1 = history[selected_run].history["precision_7"]
-      valid_pre_1 = history[selected_run].history["val_precision_7"]
-      train_pre_2 = history[selected_run].history["precision_8"]
-      valid_pre_2 = history[selected_run].history["val_precision_8"]
+      keys = list(history[selected_run].history.keys())
+      train_pre_0 = history[selected_run].history[keys[5]]
+      valid_pre_0 = history[selected_run].history[keys[13]]
+      train_pre_1 = history[selected_run].history[keys[6]]
+      valid_pre_1 = history[selected_run].history[keys[14]]
+      train_pre_2 = history[selected_run].history[keys[7]]
+      valid_pre_2 = history[selected_run].history[keys[15]]
 
-    epochs = range(len(train_pre_0)) 
+    if len(valid_pre_0) != len(train_pre_0):
+      train_pre_0 = train_pre_0[::int(len(train_pre_0)/len(valid_pre_0))]
+      train_pre_1 = train_pre_0[::int(len(train_pre_1)/len(valid_pre_1))]
+      train_pre_2 = train_pre_0[::int(len(train_pre_2)/len(valid_pre_2))]
+    epochs = range(len(valid_pre_0)) 
 
     axs[0].plot(epochs, train_pre_0)
     axs[0].plot(epochs, valid_pre_0)
