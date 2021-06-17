fbb1c955416c97cd3a04d26c180d0a50cb2e28d7
Gialbo <gilbertomanunza@gmail.com>
2021-06-17 10:21:11 +0200

    Added possibility to select plotting run

--- tools/plotter.py
+++ tools/plotter.py
@@ -7,13 +7,13 @@ Authors: Gilberto Manunza, Silvia Giammarinaro
 import matplotlib.pyplot as plt
 import json
 
-def plot_losses(history, isMCD=False):
+def plot_losses(history, isMCD=False, selected_run='3'):
   if isMCD:
     train_loss = history.history["loss"]
     valid_loss = history.history["val_loss"]
   else:
-    train_loss = history['3'].history["loss"]
-    valid_loss = history['3'].history["val_loss"]
+    train_loss = history[selected_run].history["loss"]
+    valid_loss = history[selected_run].history["val_loss"]
 
   epochs = range(len(train_loss)) 
   plt.figure(figsize=(8,6))
@@ -26,13 +26,13 @@ def plot_losses(history, isMCD=False):
     plt.ylabel("Loss")
     plt.show()
 
-def plot_accuracies(history, isMCD=False):
+def plot_accuracies(history, isMCD=False, selected_run='3'):
   if isMCD:
     train_acc = history.history["accuracy"]
     valid_acc = history.history["val_accuracy"]
   else:
-    train_acc = history['3'].history["accuracy"]
-    valid_acc = history['3'].history["val_accuracy"]
+    train_acc = history[selected_run].history["accuracy"]
+    valid_acc = history[selected_run].history["val_accuracy"]
 
   epochs = range(len(train_acc)) 
   plt.figure(figsize=(8,6))
@@ -46,7 +46,7 @@ def plot_accuracies(history, isMCD=False):
     plt.ylabel("Accuracy")
     plt.show()
 
-def plot_recalls(history, isMCD=False):
+def plot_recalls(history, isMCD=False, selected_run='3'):
   with plt.style.context('fivethirtyeight'):
     fig, axs = plt.subplots(1, 3, figsize = (15, 5))
 
@@ -58,12 +58,12 @@ def plot_recalls(history, isMCD=False):
       train_rec_2 = history.history["recall_8"]
       valid_rec_2 = history.history["val_recall_8"]
     else:
-      train_rec_0 = history['3'].history["recall_6"]
-      valid_rec_0 = history['3'].history["val_recall_6"]
-      train_rec_1 = history['3'].history["recall_7"]
-      valid_rec_1 = history['3'].history["val_recall_7"]
-      train_rec_2 = history['3'].history["recall_8"]
-      valid_rec_2 = history['3'].history["val_recall_8"]
+      train_rec_0 = history[selected_run].history["recall_6"]
+      valid_rec_0 = history[selected_run].history["val_recall_6"]
+      train_rec_1 = history[selected_run].history["recall_7"]
+      valid_rec_1 = history[selected_run].history["val_recall_7"]
+      train_rec_2 = history[selected_run].history["recall_8"]
+      valid_rec_2 = history[selected_run].history["val_recall_8"]
 
     epochs = range(len(train_rec_0)) 
 
@@ -91,7 +91,7 @@ def plot_recalls(history, isMCD=False):
 
     fig.tight_layout()
 
-def plot_precisions(history, isMCD=False):
+def plot_precisions(history, isMCD=False, selected_run='3'):
   with plt.style.context('fivethirtyeight'):
     fig, axs = plt.subplots(1, 3, figsize = (15, 5))
 
@@ -103,12 +103,12 @@ def plot_precisions(history, isMCD=False):
       train_pre_2 = history.history["precision_8"]
       valid_pre_2 = history.history["val_precision_8"]
     else:
-      train_pre_0 = history['3'].history["precision_6"]
-      valid_pre_0 = history['3'].history["val_precision_6"]
-      train_pre_1 = history['3'].history["precision_7"]
-      valid_pre_1 = history['3'].history["val_precision_7"]
-      train_pre_2 = history['3'].history["precision_8"]
-      valid_pre_2 = history['3'].history["val_precision_8"]
+      train_pre_0 = history[selected_run].history["precision_6"]
+      valid_pre_0 = history[selected_run].history["val_precision_6"]
+      train_pre_1 = history[selected_run].history["precision_7"]
+      valid_pre_1 = history[selected_run].history["val_precision_7"]
+      train_pre_2 = history[selected_run].history["precision_8"]
+      valid_pre_2 = history[selected_run].history["val_precision_8"]
 
     epochs = range(len(train_pre_0)) 
 
@@ -135,11 +135,11 @@ def plot_precisions(history, isMCD=False):
 
     fig.tight_layout()
 
-def plot_classification_report(history, isMCD=False):
-  plot_losses(history, isMCD)
-  plot_accuracies(history, isMCD)
-  plot_recalls(history, isMCD)
-  plot_precisions(history, isMCD)
+def plot_classification_report(history, isMCD=False, selected_run='3'):
+  plot_losses(history, isMCD, selected_run=selected_run)
+  plot_accuracies(history, isMCD, selected_run=selected_run)
+  plot_recalls(history, isMCD, selected_run=selected_run)
+  plot_precisions(history, isMCD, selected_run=selected_run)
 
 def save_history(history, checkpoint_dir):
   with open(f"{checkpoint_dir}.json", 'w') as fs:
